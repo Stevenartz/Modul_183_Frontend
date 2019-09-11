432136363b519d4e26fead0ec5ff6627a6b92abe
Stefan Ulrich <s.ulrich31@gmx.ch>
2019-09-11 20:57:52 +0200

    Added Dashboard fetching and nice animation

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import './Dashboard.css';
 
 class Dashboard extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {}
+        }
+    }
+
+    componentDidMount = () => {
+        let url = 'http://localhost:8080/getPersonByUsername';
+
+        let headers = new Headers();
+
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem('jwt'));
+        headers.append('Accept', 'application/json');
+        headers.append('Content-Type', 'application/json');
+
+        fetch(url, {
+            mode: 'cors',
+            method: 'GET',
+            headers: headers,
+        })
+            .then(resp => {
+                if (resp.ok) {
+                    return resp.json();
+                } else if (resp.status === 401) {
+                    throw new Error();
+                }
+            })
+            .then(data => {
+                this.setState({ user: data })
+            })
+            .catch((e) => {
+                alert("failed: " + e);
+            })
+    }
+
     render() {
         return (
-            <div>
-                Hallo Benutzer, wie ich sehe bist du eingeloggt!
+            <div className='center-screen'>
+                <div className='background'>
+                    <h1 className='fadeInText'>Hi {this.state.user.firstname} {this.state.user.lastname}!</h1>
+                    <h2 className='fade-in-top'>Ready to capture some records?</h2>
+                </div>
             </div>
         )
     }
